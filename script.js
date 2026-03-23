46c2b9165b4dd954a02cfcaf31396180159211c8
kensk <125530058+kenskstore@users.noreply.github.com>
2026-03-22 21:08:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
-let tempoRestante = 10 * 60 * 1000; // 10 minutos em milissegundos
-let msVisual = 0; // contador de 0 a 30 para efeito visual
+let tempoRestante = 5 * 60 * 1000; // 10 minutos
+let msVisual = 0;
 
 const timer = setInterval(() => {
     if (tempoRestante <= 0) {
         clearInterval(timer);
-        document.getElementById('tempo').textContent = "Oferta encerrada!";
+
+        // Remove o título antigo
+        const titulo = document.querySelector("#cronometro .titulo");
+        if (titulo) titulo.remove();
+
+        // Atualiza o contador para a mensagem final
+        const tempo = document.getElementById('tempo');
+        tempo.textContent = "OFERTA QUASE ESGOTADA!";
+        tempo.classList.add("piscar"); // adiciona a classe que faz piscar
         return;
     }
 
     let minutos = Math.floor(tempoRestante / 60000);
     let segundos = Math.floor((tempoRestante % 60000) / 1000);
 
-    minutos = minutos < 10 ? '0' + minutos : minutos;
-    segundos = segundos < 10 ? '0' + segundos : segundos;
+    minutos = minutos < 10 ? '0'+minutos : minutos;
+    segundos = segundos < 10 ? '0'+segundos : segundos;
 
-    // Milissegundos visuais de 0 a 30
-    msVisual = (msVisual + 1) % 31; 
+    msVisual = (msVisual + 1) % 31;
     let msText = msVisual < 10 ? '0'+msVisual : msVisual;
 
     document.getElementById('tempo').textContent = `${minutos}:${segundos}:${msText}`;
 
-    tempoRestante -= 33; // decrementa 33ms (~30 steps por segundo)
+    tempoRestante -= 33;
 }, 33);
+
+function copiarID(codigo, botao) {
+    navigator.clipboard.writeText(codigo)
+        .then(() => {
+            botao.innerText = "Copiado!";
+            setTimeout(() => {
+                botao.innerText = "Copiar";
+            }, 1500);
+        });
+}
